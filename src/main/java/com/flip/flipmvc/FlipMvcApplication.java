b9c9ceb086914b57f0a9b5c772af48035d9594cb
Jack Gallagher <jpgr5353@gmail.com>
2018-07-26 22:28:39 -0500

    set up initial project settings

---
+++ src/main/java/com/flip/flipmvc/FlipMvcApplication.java
@@ -0,0 +1,12 @@
+package com.flip.flipmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipMvcApplication.class, args);
+	}
+}
