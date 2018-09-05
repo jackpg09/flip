950e278cc89aa2083093e0cc386630056164f011
Jack Gallagher <jpgr5353@gmail.com>
2018-09-05 17:41:07 -0500

    nothing

--- src/main/java/com/flip/flipmvc/Models/ShoppingCart.java
+++ src/main/java/com/flip/flipmvc/Models/ShoppingCart.java
@@ -1,9 +1,12 @@
 package com.flip.flipmvc.Models;
 
+import javax.persistence.Entity;
+
 import java.util.ArrayList;
 
-
+@Entity
 public class ShoppingCart {
+
 
     private static ArrayList<MarketDisc> discsInCart = new ArrayList<>();
 
